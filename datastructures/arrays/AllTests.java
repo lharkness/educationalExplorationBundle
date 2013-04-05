9b1101e1d6bf270c3e270ce62f10525746c793c3
Lee <lee@leeharkness.comr>
2013-04-05 05:56:41 -0500

    adding ordered array

--- datastructures/arrays/AllTests.java
+++ datastructures/arrays/AllTests.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite;
 
 public class AllTests {
 	public static void main(String args[]) {
-		org.junit.runner.JUnitCore.main(HighArrayModTest.class.getName());
+		org.junit.runner.JUnitCore.main(HighArrayModTest.class.getName(), OrderedArrayTest.class.getName());
 	}
 }
